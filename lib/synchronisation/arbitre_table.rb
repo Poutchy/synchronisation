2e398c14a6b4ee67e787ce19df7107edaef9c030
Poutchy <enzo.mauti@laposte.net>
2024-02-21 08:23:27 +0100

    correction de la classe ArbitreTable

--- lib/synchronisation/arbitre_table.rb
+++ lib/synchronisation/arbitre_table.rb
@@ -4,8 +4,6 @@ require "synchronisation/arbitre"
 
 # Contiens les informations d'un arbitre de table
 class ArbitreTable < Arbitre
-  attr_reader :nom, :prenom
-
   def to_s
     "Arbitre de Terrain: #{nom} #{prenom}"
   end
