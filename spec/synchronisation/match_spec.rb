e3e4b91ed28a0c3a001017e18f5afc7ab219963d
Poutchy <enzo.mauti@laposte.net>
2024-02-21 11:22:27 +0100

    ajout de nouvelles spec pour les matchs

--- spec/synchronisation/match_spec.rb
+++ spec/synchronisation/match_spec.rb
@@ -2,12 +2,18 @@
 
 require "synchronisation/match"
 require "synchronisation/joueur"
+require "synchronisation/terrain"
+require "synchronisation/arbitre"
 
 RSpec.describe Match do
   before :each do
     @j1 = Joueur.new "Mauti", "Joris", 19
     @j2 = Joueur.new "Mauti", "Enzo", 20
-    @m = Match.new @j1, @j2
+    @a1 = Arbitre.new "Baqui", "Hanma"
+    @a2 = Arbitre.new "Baquo", "Hanmo"
+    @a3 = Arbitre.new "Baquu", "Hanmi"
+    @t = Terrain.new 1, [@a1, @a2, @a3]
+    @m = Match.new @j1, @j2, [@t]
   end
   it "can be printed" do
     res = "Match: #{@j1} VS #{@j2}\n"
